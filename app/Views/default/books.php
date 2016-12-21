e8b641f0b5164c6c701bb21f0d4d0637556a145b
clement godin <clement.godin59310@gmail.com>
2016-12-21 16:36:21 +0100

    fix bug sur les doublons de livres

--- app/Views/default/books.php
+++ app/Views/default/books.php
@@ -26,37 +26,70 @@
                             <h4><?php echo $book['author'] ?></h4>
                         </div>
                     </a>
-                <?php endforeach;
-            else : ?>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <?php
+                $liste_book=[];
+                $doublon=false;
+                $label_user=false;
+                $book_status;
+                ?>
                 <?php foreach ($books_reading_list as $book) : ?>
-                    <?php if (isset($book['book_id'])) : ?>
-                        <a href="<?php echo $this->url('public.view', ['id' => $book['book_id']]); ?>">
-                            <div class=" vignette col-xs-6 col-sm-4 col-md-2">
-                                <div class="cover">
-                                    <?php $cover = (!empty($book['cover'])) ? $book['cover'] : 'default.jpg'; ?>
-                                    <img src="<?php echo $this->assetUrl('../upload/cover') . "/" . $cover ?>"
-                                         alt="cover de <?php echo $book['title'] ?>">
-                                </div>
+                    <?php foreach ($liste_book as $id_book) : ?>
+                        <?php if ($book['book_id']==$id_book['book_id']) : ?>
+                            <?php if ($book['user_id']==$w_user['id']) : ?>
+                                <?php $label_user=true; ?>
+                                <?php $book_status=$book; ?>
+                                <?php $doublon =true; ?>
+                            <?php else: ?>
+                                <?php  $doublon=true; ?>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
 
-                                <?php if ($w_user['id']==$book['user_id']) :
-                                     if (isset($book['read_status'])):
-                                         if ($book['read_status'] == 1) : ?>
-                                            <span class="label label-success">Lu</span>
-                                        <?php else: ?>
-                                            <span class="label label-danger">Non lu</span>
-                                        <?php endif;
-                                     endif;
-                                endif; ?>
-                                <div class="titre-auteur">
-                                    <h3><?php echo $book['title'] ?></h3>
-                                    <h4><?php echo $book['author'] ?></h4>
-                                </div>
+                    <?php //var_dump($book); var_dump($liste_book); var_dump($doublon); ?>
 
-                            </div>
-                        </a>
+                    <?php if (!$doublon) : ?>
+                        <?php if (isset($book['book_id'])) : ?>
+                            <?php $liste_book[]['book_id']=$book['book_id']; ?>
+
+                            <a href="<?php echo $this->url('public.view', ['id' => $book['book_id']]); ?>">
+                                <div class=" vignette col-xs-6 col-sm-4 col-md-2">
+                                    <div class="cover">
+                                        <?php $cover = (!empty($book['cover'])) ? $book['cover'] : 'default.jpg'; ?>
+                                        <img src="<?php echo $this->assetUrl('../upload/cover') . "/" . $cover ?>"
+                                             alt="cover de <?php echo $book['title'] ?>">
+                                    </div>
+                                    <?php if ($w_user['id']==$book['user_id']) : ?>
+                                         <?php if (isset($book['read_status'])): ?>
+                                            <?php //var_dump($book); ?>
+                                            <?php if ($label_user) : ?>
+                                                <?php if ($book_status['read_status'] == 1) : ?>
+                                                    <span class="label label-success">Lu</span>
+                                                <?php else: ?>
+                                                    <span class="label label-danger">Non lu</span>
+                                                <?php endif; ?>
+                                            <?php else: ?>
+                                                <?php if ($book['read_status'] == 1) : ?>
+                                                    <span class="label label-success">Lu</span>
+                                                <?php else: ?>
+                                                    <span class="label label-danger">Non lu</span>
+                                                <?php endif; ?>
+                                            <?php endif; ?>
+                                         <?php endif; ?>
+                                    <?php endif; ?>
+                                    <div class="titre-auteur">
+                                        <h3><?php echo $book['title'] ?></h3>
+                                        <h4><?php echo $book['author'] ?></h4>
+                                    </div>
+                                </div>
+                            </a>
+                        <?php endif; ?>
+                    <?php else : ?>
+                        <?php $doublon=false; ?>
                     <?php endif; ?>
-                <?php endforeach;
-            endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
         <div class="row">
         <?php
